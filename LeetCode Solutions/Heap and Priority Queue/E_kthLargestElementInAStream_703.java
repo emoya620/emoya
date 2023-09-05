e8fb8402001e63715615d6ff4e50a6160f804c88
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2023-09-05 09:38:59 +0800

    Create E_kthLargestElementInAStream_703.java

---
+++ LeetCode Solutions/Heap and Priority Queue/E_kthLargestElementInAStream_703.java
@@ -0,0 +1,28 @@
+class KthLargest {
+    final int k;
+    final PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        for (int num : nums){
+            add(num);
+        }
+    }
+    
+    public int add(int val) {
+        if (heap.size() < k){
+            heap.offer(val);
+        }
+        else if (val > heap.peek()){
+            heap.poll();
+            heap.offer(val);
+        }
+        return heap.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
